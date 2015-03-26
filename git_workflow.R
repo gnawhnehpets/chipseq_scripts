74e5947310acccc78e485e6a7b95097fab306d95
biohack <shwang26@jhu.edu>
2015-03-26 15:19:48 -0400

    add github tut

--- git_workflow.R
+++ git_workflow.R
@@ -42,3 +42,13 @@ deleting file
 3. git rm "name of file"
 4. git commit -m 'message'
 5. git push -u origin master
+
+
+github help
+==============
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/biohack/test.git
+git push -u origin master
