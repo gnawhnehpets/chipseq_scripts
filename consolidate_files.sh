7984bedaf0b3974ea107a73ed8ab81125a88cd0c
biohack <shwang26@jhu.edu>
2015-04-08 22:47:35 -0400

    new scripts

---
+++ consolidate_files.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+##########
+# Purpose:
+##########
+
+# After creating ChIP-seq plots, this script will consolidate the files by copying all files per sample type into a separate folder; copies are made so the originals are left alone.
+
+########
+# usage:
+########
+
+# Changes should not be necessary, but make sure the folder name correspond to the sample type
+
+#FILES=(/home/steve/.gvfs/onc-analysis\$ on onc-cbio2.win.ad.jhu.edu/users/shwang26/michelle/BED_files/*)
+#for f in "${FILES[@]}"; do echo "$f"; done 
+
+#FILES=*bed
+#FILES=$(grep -rI "TEXTSEARCH") #find all text files with 'textsearch' in file
+
+mkdir -p "DNMT1"
+mkdir -p "EZH2"
+mkdir -p "H3"
+mkdir -p "H3K4"
+mkdir -p "H3K27"
+mkdir -p "Input"
+
+FILES=$(find . -name "*.txt")
+echo ${FILES}"\n"
+
+find . -name "*DNMT1*raw.jpeg" -exec cp {} ./DNMT1/ \;
+find . -name "*DNMT1*.txt" -exec cp {} ./DNMT1/ \;
+
+find . -name "*EZH2*raw.jpeg" -exec cp {} ./EZH2/ \;
+find . -name "*EZH2*.txt" -exec cp {} ./EZH2/ \;
+
+find . -name "*H3_*raw.jpeg" -exec cp {} ./H3/ \;
+find . -name "*H3_*.txt" -exec cp {} ./H3/ \;
+
+find . -name "*H3K4*raw.jpeg" -exec cp {} ./H3K4/ \;
+find . -name "*H3K4*.txt" -exec cp {} ./H3K4/ \;
+
+find . -name "*H3K27*raw.jpeg" -exec cp {} ./H3K27/ \;
+find . -name "*H3K27*.txt" -exec cp {} ./H3K27/ \;
+
+find . -name "*Input*raw.jpeg" -exec cp {} ./Input/ \;
+find . -name "*Input*.txt" -exec cp {} ./Input/ \;
+
+#for f in ${FILES}
+#do
+#    echo "FILE: $f"
+#    TOTALRAWREADS=$(wc -l < $f | grep -o "[0-9]\+")
+#    echo "total_raw_reads: ${TOTALRAWREADS}"
+#    SCALINGFACTOR=`awk 'BEGIN{printf("%0.4f", 100000000 / '$TOTALRAWREADS')}'`
+#    echo "scaling_factor:  ${SCALINGFACTOR}"
+#    echo "INPUT: ../unnormalizedBED_200bp_bin/"${f/*bin\//}"_Coverage_200bp.bed"
+#    echo "OUTPUT: "${f/*bin\//}"_Coverage_200bp_normalized_bash.BED"
+#     awk -v SF="$SCALINGFACTOR" '{print($1"\t"$2"\t"$3"\t"int($4*SF))}' ../unnormalizedBED_200bp_bin/"${f/*bin\//}"_Coverage_200bp.bed > ${f/*bin\//}"_200bp_normalized_bash.BED" 
+#done
