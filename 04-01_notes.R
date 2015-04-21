8616c63dbc7b0d364b27b37dd26a6099c9dff18a
biohack <shwang26@jhu.edu>
2015-04-21 13:15:43 -0400

    test

---
+++ 04-01_notes.R
@@ -0,0 +1,103 @@
+04/09/2015
+===================================
+replicate1=analysis that Ash has completed
+replicate2=new data
+
+
+
+Notes
+Untreated vs smoke-treated cells
+Summary: create a model in lab for chronical smoking in lung cell lining
+        tumor from this lining have genetic and epigenetic changes but do not understand well
+initial analysis - genome wide
+small bar - list of genes that are methylated; how many of those genes that are methylated are
+        k4 marked at the time point
+        basically
+in culture, over 3months, across the genome, the genes take an increase in k27
+from TSS, +/- 2500 bp
+        that wide for k27
+        k4 - wouldnt have that big of a window
+             nucleosome starts around 2kbp from TSS
+what changes here is bivalency; how did it change?
+        genes with both marks is increasing
+
+
+
+
+meeting
+=================
+10 days ezh2 increase
+
+1st iteration
+
+CHECK:
+1. is expression going down at 3M timepoint?
+2. Ash/genes- globally, methylated genes - very low expression to begin with
+- didnt look at basal level ***
+  
+  
+flaw in p16 argument
+all genes start out low exp, and then 
+p16 expressed in stem, and when cellls go into sensescnece
+
+
+cells are shipped, printed on plastic
+
+cells have no polycomb so it correlates with k4 going down
+polycomb with starting dna meth, antagonistic 
+polycomb not maintained well with highly methylation
+
+3. genes that get methylated in C but not CSC
+- we are taking genes methylated in cigarette smoke
+- are there a set of genes that get methylated in C, but not CSC - do they follow a similar pattern
+
+- denovo methylation may not be accurate because genes get a bit of methylation with age
+
+
+
+- genes specifically methylated in cig smoke
+- why is cig smoke changing the dynamic of gene methylation
+
+ezh2 methylation
+ezh2 protein catalyzes polycomb
+
+
+dnmt1 - take inverse(color) --> black to white, instead of white to black
+chip-chip - cpgi, no dnmt1
+- hypermethylated gene: before damage, they sit up stream
+- dnmt1 over TSS
+
+expand the 
+
+scripts - border around cpgi, take cpgi, sort genes, normalize size of island, and
+
+pick 7 or 8 genes, and use them as examples
+- use delta beta close to .4, and on high end (.8)
+
+***regenerate plots for highly.expressed
+UPDATE: can be found in ~/shwang26/Michelle/BED_files/Coverage_TSS_200bp_bin/normalizedBED_200bp_bin/outputdir/highly.expressed/
+- highly.expresed at 10M
+- change the color
+
+
+k4 - C to CSC, drastic increase
+
+run kates viral immune cells through***
+- gene panel already checked against expression data
+
+EMT phenotype - increase in environment expression
+- tgfbeta expression??
+
+pbs documentary - env, cig smoke
+
+
+
+
+RERUN WITH NEW NORMALIZED DATA
+==============================
+
+                    create ann bar?     stable.10M     stable.10M.new      highly-expressed    combined avg plots?      
+norm 10bp
+norm 200bp
+unnorm 10bp
+unnorm 200bp
