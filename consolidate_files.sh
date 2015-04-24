65e2f3a50ac0e1f62cdec7147635fa4f53ef5c3e
biohack <shwang26@jhu.edu>
2015-04-24 09:54:30 -0400

    code cleanup

--- consolidate_files.sh
+++ consolidate_files.sh
@@ -28,20 +28,20 @@ mkdir -p "Input"
 FILES=$(find . -name "*.txt")
 echo ${FILES}"\n"
 
-find . -name "*DNMT1*raw.jpeg" -exec cp {} ./DNMT1/ \;
+find . -name "*DNMT1*raw*.jpeg" -exec cp {} ./DNMT1/ \;
 find . -name "*DNMT1*.txt" -exec cp {} ./DNMT1/ \;
 
-find . -name "*EZH2*raw.jpeg" -exec cp {} ./EZH2/ \;
+find . -name "*EZH2*raw*.jpeg" -exec cp {} ./EZH2/ \;
 find . -name "*EZH2*.txt" -exec cp {} ./EZH2/ \;
 
-find . -name "*H3_*raw.jpeg" -exec cp {} ./H3/ \;
+find . -name "*H3_*raw*.jpeg" -exec cp {} ./H3/ \;
 find . -name "*H3_*.txt" -exec cp {} ./H3/ \;
 
-find . -name "*H3K4*raw.jpeg" -exec cp {} ./H3K4/ \;
+find . -name "*H3K4*raw*.jpeg" -exec cp {} ./H3K4/ \;
 find . -name "*H3K4*.txt" -exec cp {} ./H3K4/ \;
 
-find . -name "*H3K27*raw.jpeg" -exec cp {} ./H3K27/ \;
+find . -name "*H3K27*raw*.jpeg" -exec cp {} ./H3K27/ \;
 find . -name "*H3K27*.txt" -exec cp {} ./H3K27/ \;
 
-find . -name "*Input*raw.jpeg" -exec cp {} ./Input/ \;
+find . -name "*Input*raw*.jpeg" -exec cp {} ./Input/ \;
 find . -name "*Input*.txt" -exec cp {} ./Input/ \;
