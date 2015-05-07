50038c36b5252de338c7f2505696c3e70bf3ecd3
biohack <shwang26@jhu.edu>
2015-05-07 13:43:55 -0400

    compare generated genelists

--- compare_genelists.R
+++ compare_genelists.R
@@ -1,4 +1,3 @@
-
 system.dir="/home/steve/.gvfs/onc-analysis$ on onc-cbio2.win.ad.jhu.edu/users/shwang26/"
 # system.dir="Z:/users/shwang26/"
 # a1 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-01/rep1_ash/TSS_age.stable.hypermethylation_10M_rep1_genelist.txt"))
@@ -13,7 +12,12 @@ system.dir="/home/steve/.gvfs/onc-analysis$ on onc-cbio2.win.ad.jhu.edu/users/sh
 # intersect(a1, a2)
 
 
+###################################
+###################################
 # AGE-STABLE 
+###################################
+###################################
+
 #.2
 #rep1
 a1 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep1_agestable/TSS_age.stable.2.hypermethylation_10M_rep1_genelist.txt")))
@@ -21,9 +25,11 @@ a1 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylat
 a2 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep2_agestable/TSS_age.stable.2.hypermethylation_10M_rep2_genelist.txt")))
 dir.create(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable"))
 intersect_a1a2 <- intersect(a1, a2)
-length(a1); length(a2); length(intersect_a1a2)
+# print("rep1 age.stable .2, rep2 age.stable .2, intersect")
+# length(a1); length(a2); length(intersect_a1a2)
 a1a <- setdiff(a1, intersect_a1a2)
 a2a <- setdiff(a2, intersect_a1a2)
+print("rep1 age.stable .2, rep2 age.stable .2, intersect")
 length(a1a); length(a2a); length(intersect_a1a2)
 write.table(a1a, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1.age.stable.2.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) #row.names set to T to get the probe ids.
 write.table(a2a, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep2.age.stable.2.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) #row.names set to T to get the probe ids.
@@ -37,9 +43,11 @@ a3 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylat
 a4 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep2_agestable/TSS_age.stable.4.hypermethylation_10M_rep2_genelist.txt")))
 dir.create(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable"))
 intersect_a3a4 <- intersect(a3, a4)
-length(a3); length(a4); length(intersect_a3a4)
+# print("rep1 age.stable .4, rep2 age.stable .4, intersect")
+# length(a3); length(a4); length(intersect_a3a4)
 a3 <- setdiff(a3, intersect_a3a4)
 a4 <- setdiff(a4, intersect_a3a4)
+print("rep1 age.stable .4, rep2 age.stable .4, intersect")
 length(a3); length(a4); length(intersect_a3a4)
 write.table(a3, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1.age.stable.4.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) 
 write.table(a4, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep2.age.stable.4.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) 
@@ -48,16 +56,81 @@ write.table(intersect_a3a4, paste0(system.dir, "Michelle/MethylationData/methyla
 
 # stable.10M
 a_orig <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/new/age_stable_methylated_genes_at_10months_new_annotation.txt")))
+a_orig <- sort(unique(unlist(strsplit(a_orig, ";"))))
 length(a_orig)
 # stable.10M.new
 a_orig.new <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/new/age_stable_methylated_genes_at_10months_new_cutoff.txt")))
+a_orig.new <- sort(unique(unlist(strsplit(a_orig.new, ";"))))
 length(a_orig.new)
-
-length(a1); length(a_orig); length(intersect(a_orig, a1))
+# length(a1); length(a_orig); length(intersect(a_orig, a1))
 a1b <- setdiff(a1, a_orig)
 a_origa <- setdiff(a_orig, a1)
+print("rep1 age.stable .2, ash's list, intersect")
 length(a1b); length(a_origa); length(intersect(a_orig, a1))
 
 a3b <- setdiff(a3, a_orig.new)
 a_origb <- setdiff(a_orig.new, a3)
+print("rep1 age.stable .4, ash's list, intersect")
 length(a3b); length(a_origb); length(intersect(a_orig.new, a3))
+
+
+###################################
+###################################
+# AGE-INTEMEDIATE
+###################################
+###################################
+#.2
+#rep1
+a1 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep1_agestable/TSS_age.intermediate.2.hypermethylation_10M_rep1_genelist.txt")))
+#rep2
+a2 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep2_agestable/TSS_age.intermediate.2.hypermethylation_10M_rep2_genelist.txt")))
+dir.create(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable"))
+intersect_a1a2 <- intersect(a1, a2)
+# print("rep1 age.stable .2, rep2 age.stable .2, intersect")
+# length(a1); length(a2); length(intersect_a1a2)
+a1a <- setdiff(a1, intersect_a1a2)
+a2a <- setdiff(a2, intersect_a1a2)
+print("rep1 age.intermediate .2, rep2 age.intermediate .2, intersect")
+length(a1a); length(a2a); length(intersect_a1a2)
+write.table(a1a, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1.age.intermediate.2.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) #row.names set to T to get the probe ids.
+write.table(a2a, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep2.age.intermediate.2.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) #row.names set to T to get the probe ids.
+write.table(intersect_a1a2, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1&2.age.intermediate.2.hypermethylation_10M_common_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) #row.names set to T to get the probe ids.
+
+
+#.4
+#rep1 
+a3 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep1_agestable/TSS_age.intermediate.4.hypermethylation_10M_rep1_genelist.txt")))
+#rep2
+a4 <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/rep2_agestable/TSS_age.intermediate.4.hypermethylation_10M_rep2_genelist.txt")))
+dir.create(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable"))
+intersect_a3a4 <- intersect(a3, a4)
+# print("rep1 age.stable .4, rep2 age.stable .4, intersect")
+# length(a3); length(a4); length(intersect_a3a4)
+a3 <- setdiff(a3, intersect_a3a4)
+a4 <- setdiff(a4, intersect_a3a4)
+print("rep1 age.intermediate .4, rep2 age.intermediate .4, intersect")
+length(a3); length(a4); length(intersect_a3a4)
+write.table(a3, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1.age.intermediate.4.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) 
+write.table(a4, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep2.age.intermediate.4.hypermethylation_10M_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) 
+write.table(intersect_a3a4, paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/2015-05-04/common_agestable/TSS_rep1&2.age.intermediate.4.hypermethylation_10M_common_genelist.txt",sep=""), quote=F, row.names=F, col.names=F) 
+
+
+# intermediate.10M
+a_orig <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/new/age_intermediate_methylated_genes_at_10months_new_annotation.txt")))
+a_orig <- sort(unique(unlist(strsplit(a_orig, ";"))))
+length(a_orig)
+# intermediate.10M.new
+a_orig.new <- as.matrix(read.table(paste0(system.dir, "Michelle/MethylationData/methylatedGeneLists/new/age_intermediate_methylated_genes_at_10months_new_cutoff.txt")))
+a_orig.new <- sort(unique(unlist(strsplit(a_orig.new, ";"))))
+length(a_orig.new)
+# length(a1); length(a_orig); length(intersect(a_orig, a1))
+a1b <- setdiff(a1, a_orig)
+a_origa <- setdiff(a_orig, a1)
+print("rep1 age.intermediate .2, ash's list, intersect")
+length(a1b); length(a_origa); length(intersect(a_orig, a1))
+
+a3b <- setdiff(a3, a_orig.new)
+a_origb <- setdiff(a_orig.new, a3)
+print("rep1 age.intermediate .4, ash's list, intersect")
+length(a3b); length(a_origb); length(intersect(a_orig.new, a3))
+
